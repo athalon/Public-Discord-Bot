391d01d9ad05e366c83b88b9471bd1592a5b5d3d
athalon <emmettboehden@gmail.com>
2021-03-10 20:06:44 +0100

    Prefix Command added

--- core/helperFunctions/helperFunctions.py
+++ core/helperFunctions/helperFunctions.py
@@ -1,11 +1,21 @@
 import discord
+import json
+import os
 
-def createStandardEmbed(ctx, description, author):
+prefix_path = os.path.abspath(__file__ + "/../data/prefixes.json")
+
+def createStandardEmbed(ctx, description, title):
     embed = discord.Embed(
         description = description,
         color = 0xd303fc,
         timestamp = ctx.message.created_at
     )
-    embed.set_author(name=author, icon_url='https://cdn.discordapp.com/attachments/790920201217769513/818984229722652722/Robot_Head.jpg')
+    embed.set_author(name=title, icon_url='https://cdn.discordapp.com/attachments/790920201217769513/818984229722652722/Robot_Head.jpg')
     embed.set_footer(text=ctx.bot.user.name)
     return embed
+
+def getPrefix(client, message):
+    with open('./core/data/prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    
+    return prefixes[str(message.guild.id)]
