3eb3b48422a416b3c9adc4328e06b29975bf1c3b
athalon <emmettboehden@gmail.com>
2021-03-15 21:04:40 +0100

    Added Reddit Cog with meme command

---
+++ cogs/Reddit.py
@@ -0,0 +1,17 @@
+from discord.ext import commands
+from core.helperFunctions import *
+from random import randint
+from aiohttp import ClientSession
+
+class RedditCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def meme(self, ctx):
+        async with ClientSession() as cs:
+            async with cs.get('https://www.reddit.com/r/dankmemes/new.json?sort=hot') as r:
+                res = await r.json()
+                em = createStandardEmbed(ctx, "", "Random Meme")
+                em.set_image(url=res['data']['children'] [randint(0, 35)]['data']['url'])
+                await ctx.send(embed=em)
