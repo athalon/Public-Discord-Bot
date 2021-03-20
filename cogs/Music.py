e706114980de9498be809e78faae0be5c51eaae9
athalon <emmettboehden@gmail.com>
2021-03-20 21:27:37 +0100

    Second Music cog test: Audio downloading through youtube link

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,14 +1,31 @@
 from discord.ext import commands
 from core.helperFunctions import *
+from __future__ import unicode_literals
 import youtube_dl
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'wav',
+        'preferredquality': '192'
+    }],
+    'postprocessor_args': [
+        '-ar', '16000'
+    ],
+    'prefer_ffmpeg': True,
+    'keepvideo': True
+}
 
 class MusicCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @commands.command()
-    async def music_test(self, ctx):
-        await ctx.send("It works!")
+    async def music_test(self, ctx, link):
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+        await ctx.send("Audio downloaded!")
 
 def setup(bot):
     bot.add_cog(MusicCommands(bot))
